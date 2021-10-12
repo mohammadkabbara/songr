3ead4b785da2c13bd13e35460ffd176301804f6c
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-10-12 22:19:38 +0300

    half solve

--- src/main/java/com/songr/songr/repository/AlbumsRepository.java
+++ src/main/java/com/songr/songr/repository/AlbumsRepository.java
@@ -4,5 +4,8 @@ import com.songr.songr.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AlbumsRepository extends CrudRepository<Album, Long> {
+import java.util.Map;
+
+public interface AlbumsRepository extends CrudRepository<Album, Integer> {
+
 }
---
+++ src/main/java/com/songr/songr/repository/SongRepository.java
@@ -0,0 +1,7 @@
+package com.songr.songr.repository;
+
+import com.songr.songr.model.Song;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SongRepository extends CrudRepository<Song,Integer> {
+}
